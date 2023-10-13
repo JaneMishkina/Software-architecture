d11a319a61fc89c75c600bbfccf4ef782cd15a13
JaneMishkina <janemishkina@yandex.ru>
2023-10-13 13:51:02 +0300

    Fixed and added some methods

--- src/Homework_4/TicketProvider.java
+++ src/Homework_4/TicketProvider.java
@@ -3,10 +3,13 @@ package Homework_4;
 import java.util.List;
 
 public class TicketProvider {
+
     public List<Ticket> getTicket(int rootNumber){
+
 
         return null;
     }
+
     public boolean updateTicket(Ticket ticket){
 
         return false;
